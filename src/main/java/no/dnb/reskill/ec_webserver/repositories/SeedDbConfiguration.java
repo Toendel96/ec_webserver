bdf0f3e6beefed8acd084313f6d66dd8437a732c
Petter Tøndel <petter.toendel@outlook.com>
2021-04-06 14:32:16 +0200

    Ryddet i SeedDB klassene

--- src/main/java/no/dnb/reskill/ec_webserver/repositories/SeedDbConfiguration.java
+++ src/main/java/no/dnb/reskill/ec_webserver/repositories/SeedDbConfiguration.java
@@ -15,14 +15,15 @@ public class SeedDbConfiguration {
     public SeedDbConfiguration(DataSource dataSource) {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 
+        // ______________________
         jdbcTemplate.update("INSERT INTO User (id, username, password, user_type) VALUES (?,?,?,?)",
-                new Object[]{ 1L, "Salim","test1", "Admin"});
-
-
+                new Object[]{ 1L, "Salim","pass1", "Admin"});
+        jdbcTemplate.update("INSERT INTO User (id, username, password, user_type) VALUES (?,?,?,?)",
+                new Object[]{ 2L, "Petter","pass2", "Admin"});
+        jdbcTemplate.update("INSERT INTO User (id, username, password, user_type) VALUES (?,?,?,?)",
+                new Object[]{ 3L, "Sigbjørn","pass3", "Admin"});
 
         // ______________________
-
-
         jdbcTemplate.update("INSERT INTO Environment (id, short_name, description) VALUES (?,?,?)",
                 new Object[]{1L, "DEV", "Development environment"});
         jdbcTemplate.update("INSERT INTO Environment (id, short_name, description) VALUES (?,?,?)",
@@ -33,26 +34,13 @@ public class SeedDbConfiguration {
                 new Object[]{4L, "DISREC", "Disaster recovery"});
 
 
-
-
         // _______________
-
-
-
         jdbcTemplate.update("INSERT INTO Configuration (environment_id, key_name, value, user_id ) VALUES (?,?,?,?)",
-                new Object[]{ 1L, "Connection string for a database for application 1","",1L});
-        /*jdbcTemplate.update("INSERT INTO Configuration ( environment_id, key_name, value ) VALUES (?,?,?)",
-                new Object[]{ 1L, "Connection string for a database for application 2", 1L });
-        jdbcTemplate.update("INSERT INTO Configuration ( environment_id, key_name, value ) VALUES (?,?,?)",
-                new Object[]{ 1L, "•\tName of storage area in the cloud for application 4", 1L });
-        jdbcTemplate.update("INSERT INTO Configuration ( environment_id, key_name, value ) VALUES (?,?,?)",
-                new Object[]{ 2L, "Connection string for a database for application 3", 1L });
-        jdbcTemplate.update("INSERT INTO Configuration ( environment_id, key_name, value ) VALUES (?,?,?)",
-                new Object[]{ 3L, "•\tName of a message queue for application 4", 1L }); */
-
-
-
-        // _______________
+                new Object[]{ 1L,"Connection string for a database for application 1","",1L});
+        jdbcTemplate.update("INSERT INTO Configuration (environment_id, key_name, value, user_id ) VALUES (?,?,?,?)",
+                new Object[]{ 2L,"Connection string for a database for application 1","",2L});
+        jdbcTemplate.update("INSERT INTO Configuration (environment_id, key_name, value, user_id ) VALUES (?,?,?,?)",
+                new Object[]{ 3L,"Connection string for a database for application 1","",3L});
 
     }
 }

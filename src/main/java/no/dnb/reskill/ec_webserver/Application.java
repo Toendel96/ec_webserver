8a36169de2c229925ea44b2893dcf7582f197d33
ZiggyHowland <sigbjorn@hovland.nu>
2021-04-08 12:22:56 +0200

    Unknown changes

--- src/main/java/no/dnb/reskill/ec_webserver/Application.java
+++ src/main/java/no/dnb/reskill/ec_webserver/Application.java
@@ -36,17 +36,17 @@ public class Application {
     }
 
 
-//    @EnableWebSecurity
-//    @Configuration
-//    class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http.csrf().disable()
-//                    .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
-//                    .authorizeRequests()
-//                    .antMatchers(HttpMethod.POST, "/user").permitAll()
-//                    .anyRequest().authenticated();
-//        }
-//    }
+    @EnableWebSecurity
+    @Configuration
+    class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable()
+                    .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/user").permitAll()
+                    .anyRequest().authenticated();
+        }
+    }
 
 }

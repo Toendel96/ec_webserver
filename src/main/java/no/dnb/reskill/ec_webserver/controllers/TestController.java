ad22975dd7dcd7d812bd89abba952c9b0fc377a1
ZiggyHowland <sigbjorn@hovland.nu>
2021-04-11 21:18:36 +0200

    Added summary functionality

--- src/main/java/no/dnb/reskill/ec_webserver/controllers/TestController.java
+++ src/main/java/no/dnb/reskill/ec_webserver/controllers/TestController.java
@@ -2,21 +2,81 @@ package no.dnb.reskill.ec_webserver.controllers;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import no.dnb.reskill.ec_webserver.models.Configuration;
+import no.dnb.reskill.ec_webserver.models.Environment;
+import no.dnb.reskill.ec_webserver.models.Summary;
 import no.dnb.reskill.ec_webserver.models.User;
+import no.dnb.reskill.ec_webserver.services.ConfigurationService;
+import no.dnb.reskill.ec_webserver.services.EnvironmentService;
+import no.dnb.reskill.ec_webserver.services.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin
 public class TestController {
+
+    private EnvironmentService es;
+    private ConfigurationService cs;
+    private UserService us;
+
+    public TestController(EnvironmentService es, ConfigurationService cs, UserService us) {
+        this.es = es;
+        this.cs = cs;
+        this.us = us;
+
+    }
+
+
+    @GetMapping(
+            value="/summary",
+            produces={"application/json", "application/xml"})
+    public ResponseEntity<Collection<Summary>> analyzeAndCreateSummary() {
+        Collection<Summary> summary = new ArrayList<>();
+
+        Collection<Environment> ecol = es.findAll();
+        if (ecol.size() > 0) {
+            summary.add(new Summary("Number of environments", ecol.size()));
+
+            Collection<Configuration> ccol = cs.findAll();
+            if (ccol.size() > 0) {
+                summary.add(new Summary("Total number of configurations:", ccol.size()));
+
+                for (Environment e: ecol) {
+                    ccol = cs.findByEnvironmentId(e.getId());
+                    if (ccol.size() > 0) {
+                        summary.add(new Summary(String.format("Number of configurations for '%s'", e.getShort_name()), ccol.size()));
+                    }
+                }
+            }
+        }
+        else {
+            summary.add(new Summary("No environment or related configurations available", 0));
+        }
+
+        Collection<User> ucol = us.findAll();
+        if (ucol.size() > 0) {
+            summary.add(new Summary("\"Number of admin users registered", ucol.size()));
+        }
+
+        if (summary.size() > 0) {
+            return ResponseEntity.ok().body(summary);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
 
     @GetMapping("/hello")
     public String helloWorld(@RequestParam(value="name", defaultValue="World") String name) {
---
+++ src/main/java/no/dnb/reskill/ec_webserver/models/Summary.java
@@ -0,0 +1,15 @@
+package no.dnb.reskill.ec_webserver.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Summary {
+    private String key;
+    private int value;
+}
